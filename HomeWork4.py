43692308e31f8be2951f376cac801398caa60e06
Roman Motorin <romanchik84@gmail.com>
2024-01-30 15:01:15 +0200

    homework_4 file updated

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,3 +1,8 @@
 """1 - Є 3 группи людей(sets) australia_blacklist, poker_blacklist, alcohol_blacklist. \
 В кожній групі вказані імена. Вивести тих хто виграв джекпот(є одразу в 3х списках)"""
 
+australia_blacklist = {'Alex', 'Joe', 'Mary'}
+poker_blacklist = {'Mary', 'Linda', 'John'}
+alcohol_blacklist = {'Steven', 'Mary', 'Nick'}
+
+print(australia_blacklist)
