f5da7f27403c33923d187016d19e4e1e7403979e
Roman Motorin <romanchik84@gmail.com>
2024-01-30 15:53:53 +0200

    1st task updated

--- HomeWork_4.py
+++ HomeWork_4.py
@@ -7,8 +7,13 @@ poker_blacklist = {'Mary', 'Linda', 'John', 'Olha', 'Jason', 'Helen', 'Iryna'}
 alcohol_blacklist = {'Steven', 'Helen', 'Mary', 'Nick', 'Jack', 'Jason', 'Maryna'}
 
 names = australia_blacklist.intersection(poker_blacklist).intersection(alcohol_blacklist)
+dif_names = names.symmetric_difference(australia_blacklist).symmetric_difference(poker_blacklist).symmetric_difference(alcohol_blacklist)
 print(f'Джекпот виграли {len(names)} учасники: {", ".join(names)}!')
+print(f'Усім іншим {len(dif_names)} учасникам цього разу не пощастило. Бажаємо: {", ".join(dif_names)} успіхів наступного разу.')
 
-
+print('\n---Наступна задача---')
 """2 - Словник має наступні дані: {'Alex': 'house', 'Max': 'Flat', 'Olha': 'Appartments', 'Oleh': 'Trench'}
 Використвоючі f-string вивести: "User_name is living in place_name" для кожного юзера. Використовувати цикл"""
+
+slovnyk = {'Alex': 'house', 'Max': 'Flat', 'Olha': 'Appartments', 'Oleh': 'Trench'}
+print(slovnyk)
