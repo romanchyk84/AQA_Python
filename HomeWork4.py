37408a132b256f39975adfb1058882e745d0b8a6
Roman Motorin <romanchik84@gmail.com>
2024-01-30 15:27:55 +0200

    first task added

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,8 +1,10 @@
+Четверта домашка.
 """1 - Є 3 группи людей(sets) australia_blacklist, poker_blacklist, alcohol_blacklist. \
 В кожній групі вказані імена. Вивести тих хто виграв джекпот(є одразу в 3х списках)"""
 
-australia_blacklist = {'Alex', 'Joe', 'Mary'}
-poker_blacklist = {'Mary', 'Linda', 'John'}
-alcohol_blacklist = {'Steven', 'Mary', 'Nick'}
+australia_blacklist = {'Alex', 'Joe', 'Mary', 'Vova','Jason', 'Viktor', 'Helen'}
+poker_blacklist = {'Mary', 'Linda', 'John', 'Olha', 'Jason', 'Helen', 'Iryna'}
+alcohol_blacklist = {'Steven', 'Helen', 'Mary', 'Nick', 'Jack', 'Jason', 'Maryna'}
 
-print(australia_blacklist)
+names = australia_blacklist.intersection(poker_blacklist).intersection(alcohol_blacklist)
+print(f'Джекпот виграли {len(names)} учасники: {", ".join(names)}!')
