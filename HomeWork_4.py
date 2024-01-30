fb8d9a50af523a210265de6a8336d902ee0a370d
Roman Motorin <romanchik84@gmail.com>
2024-01-30 15:29:32 +0200

    file renamed and first task added

--- HomeWork_4.py
+++ HomeWork_4.py
@@ -1,4 +1,4 @@
-Четверта домашка.
+# Четверта домашка. https://github.com/romanchyk84/AQA_Python.git
 """1 - Є 3 группи людей(sets) australia_blacklist, poker_blacklist, alcohol_blacklist. \
 В кожній групі вказані імена. Вивести тих хто виграв джекпот(є одразу в 3х списках)"""
 
