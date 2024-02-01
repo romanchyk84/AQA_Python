2d8a8e8fde3b8315c9f828952424c21a87620539
Roman Motorin <romanchik84@gmail.com>
2024-02-01 15:14:47 +0200

    domashka 5

--- HomeWork_5.py
+++ HomeWork_5.py
@@ -4,12 +4,3 @@
 # dict2 = dict(map(lambda x: (int(x), int(x)**2), str_list))
 # print(dict2)
 
-# from copy import deepcopy
-#
-# list1 = [1, 2, 3, 4, 5, 6, {1: 2, 2: 5}, 'z', 'y']
-# prod1 = list1
-# list2 = deepcopy(list1)
-# list1[6][1] = 9
-# list2.append('w')
-# print(id(list1[6]), list1[6])
-# print(id(list2[6]), list2[6])
